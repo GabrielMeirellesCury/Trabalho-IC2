504f7f39b625b6359150777b59658daab2552f3b
Gabriel Meirelles Cury <curymgabriel@gmail.com>
2025-11-21 15:22:32 -0300

    coloquei os arquivos

--- Trabalho1-ic2.cpp
+++ Trabalho1-ic2.cpp
@@ -1,16 +1,51 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 
-//métodos de ordenação
+//----------leiura de arquivos----------
+void lerArquivo(fstream *arqAno, string ano[], int tamanho)
+{
+
+}
 
 
+//----------métodos de ordenação----------
+
+//inserção direta
+
+
+
+//inserção binaria
+
+
+//seleção
+
+
+//bubblesort
+
+
+//heapsort
+
+
+//fusao
+
+
+ //quicksort
+
+
+
+
+
+ //----------Busca binária----------
 
 
 
 int main()
 {
-
-
+    fstream ano1, ano2, ano3, ano4, ano5;
+    ano1.open("ano_1.txt");
+    
     return 0;
 }
